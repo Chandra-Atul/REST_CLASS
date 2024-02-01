3e1b62b2f3815d95a8f1449c9137da9f7569beb1
Atul Chandra <atulchandra021@gmail.com>
2024-02-01 14:36:46 +0530

    error in patch (editing the post)

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ uuidv4();
 
 
 app.use(express.urlencoded({extended: true}));
+app.use(express.json());
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
@@ -35,8 +36,6 @@ let posts = [
 ];
 
 
-
-
 app.get("/posts",(req, res)=>{
     res.render("index.ejs", {posts});
 });
@@ -52,39 +51,25 @@ app.post("/posts", (req, res)=>{
     res.redirect("/posts");
 });
 
-
-// app.patch("/posts/:id", (req, res) =>{
-//     let {id} = req.params;
-//     console.log(id);
-//     res.send("patch request working");
-// })
-
-
-
 app.get("/posts/:id", (req, res) => {
     let {id} = req.params;
     let post = posts.find((p) => id === p.id);
     res.render("show.ejs", {post}); 
 });
 
-// --------------------code for deletion of a post
 
 
-// app.delete("/posts/:id", (req, res) => {
-//     let {id} = req.params;
-//     posts = posts.filter((p) => id !== p.id);
-//     // res.send("delete success");
-//     res.redirect("/posts");
-
-// });
-
-
-app.delete("/posts/:id", (req, res)=>{
+app.patch("/posts/:id", (req, res) =>{
     let {id} = req.params;
-    let post = posts.find((p) => id === p.id);
-    res.send("delete success");
-})
 
+    let newContent = req.body.content;
+    console.log("newContent is: ",newContent);
+    let post = posts.find((p)=>id === p.id);
+    post.content = newContent;
+    console.log(post);
+
+    res.send("patch request working");
+});
 
 app.listen(port, ()=>{
     console.log(`listening to port:  ${port}`);
